3c0a68bb968e7cd0ccbdde6541e32c4c71149fe0
DylanLord4real <dylan.adekpe@modelis-tech.com>
2026-08-06 20:26:53 +0000

    algo complexity

---
+++ algo/binarySearch.js
@@ -0,0 +1,17 @@
+/*
+ * Given a sorted array, find the index of an element
+ * using a binary search algorithm.
+ *
+ * Example usage:
+ *
+ * var index = binarySearch([1, 2, 3, 4, 5], 4);
+ * console.log(index); // [3]
+ */
+
+var binarySearch = function(arr, element) {
+       return arr.indexOf(element)
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7))
+
+ 
